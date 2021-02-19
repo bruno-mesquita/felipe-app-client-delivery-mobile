8d226746c009212eed0ca26d3e450ff9dcf47ce9
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-02-19 13:46:54 -0300

    feat(started project now) first configurations

---
+++ src/@types/index.d.ts
@@ -0,0 +1,7 @@
+// Para conseguir importar imagens na aplicação
+
+declare module "*.png" // Todo arquivo .png
+
+declare module "*.png" // Todo arquivo .svg
+
+declare module "*.png" // Todo arquivo .jpg
