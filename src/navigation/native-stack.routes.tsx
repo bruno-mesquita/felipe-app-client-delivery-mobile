cdf064fcfb91d1eb0b3a4f6654076ca1212e5d3a
Bruno Mesquita <silvabruno.git@gmail.com>
2021-03-04 23:10:48 -0300

    #feat header com logo

--- src/navigation/native-stack.routes.tsx
+++ src/navigation/native-stack.routes.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
-
 import { createStackNavigator } from '@react-navigation/stack';
+import { Image } from 'react-native';
 
 import Home from '../screens/home';
 import Profile from '../screens/profile';
 
 const { Navigator, Screen } = createStackNavigator();
 
+const Header = () => (
+  <Image
+    style={{ width: 90, height: 43 }}
+    source={require('../assets/images/logo.png')}
+  />
+);
+
 export default function NativeStackRoutes() {
   return (
-    <Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-      <Screen name="Home" component={Home} />
+    <Navigator initialRouteName="Home">
+      <Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#9E0404',
+          },
+          headerTitle: () => <Header />,
+        }}
+      />
       <Screen name="Profile" component={Profile} />
     </Navigator>
   );
