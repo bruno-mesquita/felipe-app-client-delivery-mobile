bbec27bc5b8ea917ddcd25db94c1260a9fa7d698
Bruno Mesquita <silvabruno.git@gmail.com>
2021-04-15 20:26:50 -0300

    #fix nome do field incorreto

--- src/screens/NotAuth/ConfirmPassword/index.tsx
+++ src/screens/NotAuth/ConfirmPassword/index.tsx
@@ -55,10 +55,10 @@ const CodeToPassword = () => {
                 <Field
                   value={values.newPassword}
                   placeholder="Nova senha"
-                  onChangeText={handleChange('password')}
+                  onChangeText={handleChange('newPassword')}
                   label="Nova senha"
                 />
-                <ErrorMessage component={Text} name="password" />
+                <ErrorMessage component={Text} name="newPassword" />
               </ContainerInput>
 
               <ContainerInput>
