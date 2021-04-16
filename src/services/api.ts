d218c24fea47c5e418e1078792bf0df3a4694308
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-04-16 19:58:29 -0300

    service api do flipp

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.107:3333/api/app',
+  baseURL: 'https://app-backend-felipe.herokuapp.com/api',
 });
 
 export default api;
