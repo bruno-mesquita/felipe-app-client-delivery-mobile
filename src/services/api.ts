e18c3a567b8f320cc700dbab828e1bae6becece0
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-04-16 20:58:33 -0300

    fix (alteraçao da rota api em service para deploy)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://app-backend-felipe.herokuapp.com/api',
+  baseURL: 'https://app-backend-felipe.herokuapp.com/api/app',
 });
 
 export default api;
