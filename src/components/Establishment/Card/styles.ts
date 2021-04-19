ada83beae87f7186f0d5bd00584403ece51c5187
Bruno Mesquita <silvabruno.git@gmail.com>
2021-04-19 19:05:20 -0300

    #[Frontend][Bug] Card de produto está com a imagem deslocada dependendo do celular

--- src/components/Establishment/Card/styles.ts
+++ src/components/Establishment/Card/styles.ts
@@ -1,11 +1,8 @@
 import styled from 'styled-components/native';
-import { Dimensions } from 'react-native';
-
-const { width } = Dimensions.get('screen');
 
 export const Container = styled.View`
-  width: ${width * 0.8}px;
-  justify-content: space-around;
+  width: 100%;
+  justify-content: space-between;
   flex-direction: row;
   height: 100%;
 `;
