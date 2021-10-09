de07b6444f2ea1c0cad76ce3bbb849e65ed05f12
Bruno Mesquita <silvabruno.git@gmail.com>
2021-10-08 21:22:46 -0300

    #fix nome do app

--- src/screens/TermsUse/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native';
-import Constants from 'expo-constants';
-
-export const Container = styled.View`
-  /* flex: 1;
-  justify-content: center;
-  align-items: center; */
-  /* padding-top: ${Constants.statusBarHeight * 2}px; */
-`;
