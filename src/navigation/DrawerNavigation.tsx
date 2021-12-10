90e444744f4b27787dd8ce194cfd20eaabeb73ae
Bruno Mesquita <silvabruno.git@gmail.com>
2021-12-10 01:15:52 -0300

    #fix nome de tela duplicado

--- src/navigation/DrawerNavigation.tsx
+++ src/navigation/DrawerNavigation.tsx
@@ -6,7 +6,10 @@ import { Drawer } from '../components/Drawer';
 const { Navigator, Screen } = createDrawerNavigator();
 
 export const DrawerNavigation = () => (
-  <Navigator screenOptions={{ headerShown: false }} drawerContent={props => <Drawer {...props} />}>
-    <Screen name="Home" component={StackAuthNavigation} />
+  <Navigator
+    screenOptions={{ headerShown: false }}
+    drawerContent={props => <Drawer {...props} />}
+  >
+    <Screen name="StackAuth" component={StackAuthNavigation} />
   </Navigator>
 );
--- src/navigation/StackAuthNavigation.tsx
+++ src/navigation/StackAuthNavigation.tsx
@@ -25,10 +25,21 @@ import {
 
 const { Navigator, Screen } = createNativeStackNavigator<ScreenAuthList>();
 
-const Header = () => <Image style={{ width: 90, height: 43 }} source={require('../assets/images/logo.png')} />;
+const Header = () => (
+  <Image
+    style={{ width: 90, height: 43 }}
+    source={require('../assets/images/logo.png')}
+  />
+);
 
 const Menu = ({ openDrawer }) => (
-  <MaterialIcons name="menu" size={25} color="#fff" style={{ paddingLeft: 10 }} onPress={openDrawer} />
+  <MaterialIcons
+    name="menu"
+    size={25}
+    color="#fff"
+    style={{ paddingLeft: 10 }}
+    onPress={openDrawer}
+  />
 );
 
 const headerOptions = (name: string) =>
@@ -63,11 +74,31 @@ export const StackAuthNavigation = ({ navigation }) => (
         headerLeft: () => <Menu openDrawer={navigation.openDrawer} />,
       }}
     />
-    <Screen name="Profile" component={Profile} options={headerOptions('Perfil')} />
-    <Screen name="ChangePassword" component={ChangePassword} options={headerOptions('Alterar senha')} />
-    <Screen name="Adresses" component={Adresses} options={headerOptions('Endereços')} />
-    <Screen name="AddAddress" component={AddAddress} options={headerOptions('Adicionar endereço')} />
-    <Screen name="UpdateAddress" component={UpdateAddress} options={headerOptions('Atualizar endereço')} />
+    <Screen
+      name="Profile"
+      component={Profile}
+      options={headerOptions('Perfil')}
+    />
+    <Screen
+      name="ChangePassword"
+      component={ChangePassword}
+      options={headerOptions('Alterar senha')}
+    />
+    <Screen
+      name="Adresses"
+      component={Adresses}
+      options={headerOptions('Endereços')}
+    />
+    <Screen
+      name="AddAddress"
+      component={AddAddress}
+      options={headerOptions('Adicionar endereço')}
+    />
+    <Screen
+      name="UpdateAddress"
+      component={UpdateAddress}
+      options={headerOptions('Atualizar endereço')}
+    />
 
     <Screen
       name="Establishment"
@@ -75,13 +106,33 @@ export const StackAuthNavigation = ({ navigation }) => (
       options={{ ...headerOptions(''), headerTitle: () => <Header /> }}
     />
 
-    <Screen name="Deliverymans" component={Deliveryman} options={headerOptions('Motoboys')} />
+    <Screen
+      name="Deliverymans"
+      component={Deliveryman}
+      options={headerOptions('Motoboys')}
+    />
 
     <Screen name="Cart" component={Cart} options={headerOptions('Carrinho')} />
-    <Screen name="TrackOrder" component={TrackOrder} options={headerOptions('Acompanhar pedido')} />
-    <Screen name="Orders" component={Orders} options={headerOptions('Pedidos')} />
-    <Screen name="Configuration" component={Configuration} options={headerOptions('Configurações')} />
-    <Screen name="TermsUse" component={TermsUse} options={headerOptions('Termos de uso')} />
+    <Screen
+      name="TrackOrder"
+      component={TrackOrder}
+      options={headerOptions('Acompanhar pedido')}
+    />
+    <Screen
+      name="Orders"
+      component={Orders}
+      options={headerOptions('Pedidos')}
+    />
+    <Screen
+      name="Configuration"
+      component={Configuration}
+      options={headerOptions('Configurações')}
+    />
+    <Screen
+      name="TermsUse"
+      component={TermsUse}
+      options={headerOptions('Termos de uso')}
+    />
     <Screen
       name="AboutApp"
       component={AboutApp}
@@ -89,6 +140,10 @@ export const StackAuthNavigation = ({ navigation }) => (
         headerShown: false,
       }}
     />
-    <Screen name="Account" component={Account} options={headerOptions('Minha conta')} />
+    <Screen
+      name="Account"
+      component={Account}
+      options={headerOptions('Minha conta')}
+    />
   </Navigator>
 );
