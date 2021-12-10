288398531741a7454f618dc1f04170071ae9af90
Bruno Mesquita <silvabruno.git@gmail.com>
2021-12-10 01:24:25 -0300

    #fix url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,8 +7,8 @@ import {
   refreshTokenAction,
 } from '../store/ducks/auth/auth.actions';
 
-// const baseURL = 'https://api.flippdelivery.com.br/api/app';
-const baseURL = 'http://192.168.15.24:3030/api/app';
+const baseURL = 'https://api.flippdelivery.com.br/api/app';
+// const baseURL = 'http://192.168.15.24:3030/api/app';
 
 const api = axios.create({
   baseURL: baseURL,
