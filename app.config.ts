d62bd957918dd8dd1b7b86da264cd5ebb44c2aee
Bruno Mesquita <silvabruno.git@gmail.com>
2021-12-19 17:33:18 -0300

    #feat habilitando hermes

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+import { ExpoConfig, ConfigContext } from '@expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: config.name,
+  slug: config.slug,
+  extra: {
+    apiUrl: process.env.API_URL ?? 'http://192.168.15.24:3030/api/app',
+  },
+});
