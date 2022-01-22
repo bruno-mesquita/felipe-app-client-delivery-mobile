31755c51ba4db7559357e2df2af2899a54da22b1
Bruno Mesquita <silvabruno.git@gmail.com>
2022-01-22 02:54:12 -0300

    #feat botao do carrinho com nativebase

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -1,26 +1,29 @@
-import { Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { useTheme } from 'styled-components/native';
 import { useNavigation } from '@react-navigation/native';
+import { Fab, Icon } from 'native-base';
 
-import { NavigationAuthHook } from '@utils/ScreenProps';
 import { useAppSelector } from '@store/hooks';
 
-import { ButtonAdd } from './styles';
-
 export const CartButton = () => {
-  const { colors } = useTheme();
-  const navigation = useNavigation<NavigationAuthHook<any>>();
+  const navigation = useNavigation();
   const cartEmpty = useAppSelector(({ cart }) => cart.items.length === 0);
 
   return (
     <>
       {!cartEmpty && (
-        <ButtonAdd onPress={() => navigation.navigate('Cart')}>
-          <Text>
-            <Ionicons name="cart-outline" size={30} color={colors.secundary} />
-          </Text>
-        </ButtonAdd>
+        <Fab
+          bg="#9E0404"
+          onPress={() => navigation.navigate('Cart')}
+          position="absolute"
+          size="sm"
+          icon={
+            <Icon
+              color="white"
+              as={<Ionicons name="cart-outline" />}
+              size="sm"
+            />
+          }
+        />
       )}
     </>
   );
--- src/components/CartButton/props.ts
+++
@@ -1,3 +0,0 @@
-export interface CartButtonProps {
-  onPress: () => void;
-}
