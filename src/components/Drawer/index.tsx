159b562fce52b7ef3c54e82f139548fa1cc72c35
Bruno Mesquita <silvabruno.git@gmail.com>
2022-02-05 18:36:37 -0300

    #chore refactor no drawer com native-base

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,24 +1,13 @@
 import { DrawerContentComponentProps } from '@react-navigation/drawer';
 import { MaterialIcons } from '@expo/vector-icons';
+import { Pressable, Flex, Text, Divider } from 'native-base';
 
+import { FastImage } from '../FastImage';
 import { useAppDispatch } from '@store/hooks';
 import { useUser } from '@hooks';
 import { authActions } from '@store/reducers/auth';
 
-import {
-  Container,
-  List,
-  ListItem,
-  ListItemText,
-  User,
-  UserAvatar,
-  Divider,
-} from './styles';
-
-export const Drawer = ({
-  navigation,
-  ...props
-}: DrawerContentComponentProps) => {
+export const Drawer = ({ navigation }: DrawerContentComponentProps) => {
   const dispatch = useAppDispatch();
 
   const { user } = useUser();
@@ -36,39 +25,59 @@ export const Drawer = ({
   const goCategories = () => navigation.navigate('Categories');
 
   return (
-    <Container {...props}>
-      <User>
-        {user.avatar ? (
-          <UserAvatar source={{ uri: user.avatar }} />
-        ) : (
-          <MaterialIcons name="account-circle" size={125} color="#c4c4c4" />
-        )}
-      </User>
-      <List>
-        <ListItem onPress={goProfile}>
-          <ListItemText>Perfil</ListItemText>
-          <Divider />
-        </ListItem>
-        <ListItem onPress={goCategories}>
-          <ListItemText>Categorias</ListItemText>
-          <Divider />
-        </ListItem>
-        <ListItem onPress={goOrders}>
-          <ListItemText>Pedidos</ListItemText>
-          <Divider />
-        </ListItem>
-        <ListItem onPress={goAdresses}>
-          <ListItemText>Endereços</ListItemText>
-          <Divider />
-        </ListItem>
-        <ListItem onPress={goConfiguration}>
-          <ListItemText>Configurações</ListItemText>
-          <Divider />
-        </ListItem>
-        <ListItem onPress={logout}>
-          <ListItemText>Sair</ListItemText>
-        </ListItem>
-      </List>
-    </Container>
+    <Flex safeAreaTop bg="#9E0404" flex={1} px="20px">
+      {user.avatar ? (
+        <FastImage
+          alignSelf="center"
+          my="20px"
+          cacheKey={user.id.toString()}
+          size="125px"
+          rounded="100px"
+          source={{ uri: user.avatar }}
+        />
+      ) : (
+        <MaterialIcons
+          name="account-circle"
+          size={125}
+          color="#c4c4c4"
+          style={{ alignSelf: 'center', marginVertical: 20 }}
+        />
+      )}
+      <Pressable my="5px" onPress={goProfile}>
+        <Text color="#fff" fontSize="17px">
+          Perfil
+        </Text>
+        <Divider my="8px" />
+      </Pressable>
+      <Pressable my="5px" onPress={goCategories}>
+        <Text color="#fff" fontSize="17px">
+          Categorias
+        </Text>
+        <Divider my="8px" />
+      </Pressable>
+      <Pressable my="5px" onPress={goOrders}>
+        <Text color="#fff" fontSize="17px">
+          Pedidos
+        </Text>
+        <Divider my="8px" />
+      </Pressable>
+      <Pressable my="5px" onPress={goAdresses}>
+        <Text color="#fff" fontSize="17px">
+          Endereços
+        </Text>
+        <Divider my="8px" />
+      </Pressable>
+      <Pressable my="5px" onPress={goConfiguration}>
+        <Text color="#fff" fontSize="17px">
+          Configurações
+        </Text>
+        <Divider my="8px" />
+      </Pressable>
+      <Pressable my="5px" onPress={logout}>
+        <Text color="#fff" fontSize="17px">
+          Sair
+        </Text>
+      </Pressable>
+    </Flex>
   );
 };
