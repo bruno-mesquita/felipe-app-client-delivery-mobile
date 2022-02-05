c2e54579ee71fa1c3ef06bf542bd8fa612478673
Bruno Mesquita <silvabruno.git@gmail.com>
2022-02-05 18:40:38 -0300

    #fix efeito visual para rodar para lado no carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,29 +1,25 @@
 import { useCallback } from 'react';
-import { FlatList, useWindowDimensions } from 'react-native';
-import { Flex } from 'native-base';
+import { FlatList } from 'react-native';
 
 import { useGetAnnouncements } from '@hooks';
 
 import { FastImage } from '../FastImage';
 
 export const Carousel = () => {
-  const { width } = useWindowDimensions();
-
   const { announcements } = useGetAnnouncements();
 
   const _renderItem = useCallback(
     (
       { item } // TODO Link
     ) => (
-      <Flex w={width} align="center">
-        <FastImage
-          w="90%"
-          h="200px"
-          rounded="20px"
-          source={{ uri: item.photo.encoded }}
-          cacheKey={item.id.toString()}
-        />
-      </Flex>
+      <FastImage
+        w="280px"
+        h="150px"
+        rounded="20px"
+        source={{ uri: item.photo.encoded }}
+        cacheKey={item.id.toString()}
+        mr="20px"
+      />
     ),
     []
   );
@@ -34,10 +30,10 @@ export const Carousel = () => {
   return (
     <FlatList
       style={{ flexGrow: 0 }}
+      contentContainerStyle={{ paddingLeft: 20 }}
       horizontal
       showsHorizontalScrollIndicator={false}
       data={announcements}
-      pagingEnabled
       renderItem={_renderItem}
       keyExtractor={keyExtractor}
     />
